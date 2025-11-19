ec8565bed6af5601b84c78cd7764b30f2c6481ad
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-11-18 21:05:47 -0300

    fix: remove modificadores

--- documentationApi/Interfaces/IDocumentationRepository.cs
+++ documentationApi/Interfaces/IDocumentationRepository.cs
@@ -4,9 +4,9 @@ namespace documentationApi.Interfaces
 {
     public interface IDocumentationRepository
     {
-        public Task<List<Documentation>> ListAsync();
+        Task<List<Documentation>> ListAsync();
         Task<Documentation?> FindAsync(int id);
-        public Task<Documentation> AddAsync(Documentation doc);
-        public Task<bool> Remove(int id);
+        Task<Documentation> AddAsync(Documentation doc);
+        Task<bool> Remove(int id);
     }
 }
